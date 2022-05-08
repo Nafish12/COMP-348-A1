feba392f6943c82b1485f90569b385a4a0bf9fcb
Nafish12 <md.nafisali1@gmail.com>
2022-05-08 16:07:07 -0400

    PrintObject abstract class added

--- A1/src/Printable.java
+++ A1/src/Printable.java
@@ -1,8 +1,11 @@
 import java.util.*;
 
 public interface Printable {
+	//a void method to print object’s info to the console.
 	public void print();
 	
+	//A static print() method that receives a list of printables and calls their print()
+	//methods3
 	public static void print(List <Printable> x) {
 		print(x);
 	}
--- A1/src/PrintableObject.java
+++ A1/src/PrintableObject.java
@@ -1,9 +1,15 @@
 
 public abstract class PrintableObject implements NamedObject, Printable {
-
+	//overridden, returns the object’s name by calling getName() method
+	public String toString() {
+		return this.getName();
+	}
+	
+	//prints the text returned by toString() method. The information is
+	//printed in single line.
 	@Override
 	public void print() {
-		// TODO Auto-generated method stub
+		System.out.println(toString());
 
 	}
 
