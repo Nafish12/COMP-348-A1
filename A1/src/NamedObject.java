348d524a2e3b3577dc9e554c02f071a0088f07c7
Nafish12 <md.nafisali1@gmail.com>
2022-05-08 15:44:36 -0400

    Shape interface addded

--- A1/src/NamedObject.java
+++ A1/src/NamedObject.java
@@ -1,5 +1,9 @@
 
 public interface NamedObject {
+	/*
+	 * a method to return object’s name. Provide a default implementation
+	 * that returns the object’s Runtime class name2
+	 */
 	public default String getName() {
 		return this.getClass().getSimpleName();
 	}
---
+++ A1/src/Shape.java
@@ -0,0 +1,7 @@
+
+public interface Shape extends NamedObject {
+	//a method to return shape’s perimeter
+	public double getPerimeter();
+	//a method to return shape’s area
+	public double getArea();
+}
